1c38a594492c832246ccf8cebc38633fcd8451a0
hemanthreddy-45 <hemanth.kumar@opsera.io>
2024-05-23 21:08:28 +0530

    latest commit

--- demo.sh
+++ demo.sh
@@ -2,3 +2,10 @@ token = "P$5aRt#2oN!9lM@8as"
 token = P$5aRt#2oN!9lM@8as
 aws_s3_secret_key = s3_raw_secret_key
 AWS_ACCESS_KEY="JlZC3i8nCmzdL7lQZQ"
+credentials = {
+    "aws_s3_secret_key": "s3_raw_secret_key",
+    "aws_access_key_id": "your_access_key_id",
+    "aws_region": "your_aws_region",
+    "bucket_name": "your_bucket_name",
+    "file_path": "your_file_path"
+}

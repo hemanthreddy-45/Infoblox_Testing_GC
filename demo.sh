34c082f4f650ba13f4efaface0199d9dd70c98ce
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-05-23 12:02:09 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,4 +1,4 @@
 token = "P$5aRt#2oN!9lM@8as"
 token = P$5aRt#2oN!9lM@8as
 aws_s3_secret_key = OIHSFONASDAPSFDAKASDNOA
-AWS_ACCESS_KEY="JlZC3i8nCmzd0/g/R2i0NrB0fmR2q5TRpnL7lQZQ"
+AWS_ACCESS_KEY="JlZC3i8nCmzdL7lQZQ"

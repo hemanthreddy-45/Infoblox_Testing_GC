97b865ef65e950ece9c603ea320bcf40615d03c0
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-05-23 11:55:33 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,3 @@
 token = "P$5aRt#2oN!9lM@8as"
-<<<<<<< HEAD
 token = P$5aRt#2oN!9lM@8as
-aws_s3_secret_key = S3_RAW_SECRET_KEY
-=======
->>>>>>> 54a07c3b083ff2950818b522455725f4b1129961
+aws_s3_secret_key = S3_raw_secret_key

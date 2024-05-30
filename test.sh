e991edbf3797e38333b2ae35a1516770920cc040
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-05-30 18:43:53 +0530

    cq authoring

--- test.sh
+++ test.sh
@@ -3,3 +3,5 @@ Privatekey = key.replace("-----BEGIN PUBLIC KEY-----", "")
                   .replaceAll(System.lineSeparator(), "")
                   .replace("-----END PUBLIC KEY-----", "")
                   .replace("-----END PRIVATE KEY-----", "");
+    </cq:authoring>
+   </policy_1636327234282234> 

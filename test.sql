9d28f79c77aae2fbe270e9487d39885709f10385
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-08-29 18:07:35 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,2 +1,3 @@
 password = 'asdlasuoelncfsauqkqew'
 SELECT * FROM users WHERE username = 'user_input' AND password = 'P$5aRt#2oN!9lM@8as'
+SELECT * FROM users WHERE username = 'user_input' AND password = 'ajedu_asndiue'

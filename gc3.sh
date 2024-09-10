413a57deb42a3056c0a693de02b17acb3feccaaf
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-09-10 13:19:23 +0530

    Update gc3.sh

--- gc3.sh
+++ gc3.sh
@@ -3,3 +3,6 @@ token = "P$5aRt#2oN!9lM@8as"
 token = P$5aRt#2oN!9lM@8as
 aws_s3_secret_key = s3_raw_secret_key
 AWS_ACCESS_KEY="JlZC3i8nCmzdL7lQZQ"
+password = 'asdlasuoelncfsauqkqew'
+SELECT * FROM users WHERE username = 'user_input' AND password = 'P$5aRt#2oN!9lM@8as'
+SELECT * FROM users WHERE username = 'user_input' AND password = 'eyHKSADdu_asndiue'

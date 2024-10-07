b511c7fbb7fdd725d0986a8935e7ccb1510a7759
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-10-07 21:10:57 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -2,6 +2,8 @@ token = "P$5aRt#2oN!9lM@8as"
 token = P$5aRt#2oN!9lM@8as
 aws_s3_secret_key = s3_raw_secret_key
 AWS_ACCESS_KEY="JlZC3i8nCmzdL7lQZQ"
+askdasd
+asiirenadlsjdsadasd
 credentials = {
     "aws_access_key_id": "your_access_key_id",
     "aws_region": "your_aws_region",
